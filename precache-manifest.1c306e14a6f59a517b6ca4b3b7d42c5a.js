bd3e90a7fc9f4e8c8323f86c090d7c44d3a640d9
bond-st <56007001+bond-st@users.noreply.github.com>
2019-11-17 14:56:40 +1300

    Update precache-manifest.1c306e14a6f59a517b6ca4b3b7d42c5a.js

--- precache-manifest.1c306e14a6f59a517b6ca4b3b7d42c5a.js
+++ precache-manifest.1c306e14a6f59a517b6ca4b3b7d42c5a.js
@@ -5,14 +5,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "625f391bc6e135afb33b",
-    "url": "/bond/static/js/2.f0225e8a.chunk.js"
+    "url": "/static/js/2.f0225e8a.chunk.js"
   },
   {
     "revision": "2212b75f12e5b4b6e489",
-    "url": "/bond/static/js/main.e6c487d4.chunk.js"
+    "url": "/static/js/main.e6c487d4.chunk.js"
   },
   {
     "revision": "2bcad48dd0baf145ff39",
-    "url": "/bond/static/js/runtime-main.aa8345a6.js"
+    "url": "/static/js/runtime-main.aa8345a6.js"
   }
 ]);
