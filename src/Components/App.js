e0ec54856209723d0540d093280b955cef96492a
Jonah <Jonahease@gmail.com>
2024-09-26 10:49:22 +1000

    WIP

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,22 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import Edit from './Edit'
-import Unit from './Unit'
-import Misc from './Misc'
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Edit from "./Edit";
+import Unit from "./Unit";
+import Misc from "./Misc";
 
 function App() {
   return (
     <Router>
-    <div className='app-container'>
-      {/* routing what files display on different url paths */}
-      <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route path='/edit' element={<Edit/>} />
-        <Route path='/unit' element={<Unit/>} />
-        <Route path='/misc' element={<Misc/>} />
-      </Routes>
-    </div>
-  </Router>
+      <div className="app-container">
+        {/* routing what files display on different url paths */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/unit" element={<Unit />} />
+          <Route path="/misc" element={<Misc />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
